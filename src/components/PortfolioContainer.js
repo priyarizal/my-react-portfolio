36ce2f4cedac289f4078b4abb9fdfd281496545a
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-09-28 12:49:05 -0700

    reshuffling pages and components

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import NavTabs from './Navbar';
-import Home from './Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
-import Projects from './pages/Projects';
+import Contact from './pages/Projects';
+import Projects from './pages/Resume';
+import Resume from './pages/Contact';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
     if (currentPage === 'About') {
       return <About />;
     }
     if (currentPage === 'Projects') {
       return <Projects />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
     return <Contact />;
   };
